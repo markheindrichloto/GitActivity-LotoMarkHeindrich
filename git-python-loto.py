e08751ff9811367be4e4e990d1fa9c5a891ce769
LotoMarkHeindrich <markheindrichloto@gmail.com>
2024-02-15 17:08:08 +0800

    My Personal Info added

---
+++ git-python-loto.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 16:59:56 2024
+
+@author: Mark Heindrich B. Loto
+"""
+
+print("Name: Mark Heindrich B. loto")
+print("Address: Cawit, Boac, Marinduque")
+print("Birthday: January 24, 2003")
+
